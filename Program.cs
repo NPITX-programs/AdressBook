026b761a804bd579116c8edda45d89de4194d622
CTH-npitx <chatch@npitx.org>
2025-04-21 10:21:19 -0500

    initial component of code

--- Program.cs
+++ Program.cs
@@ -18,5 +18,6 @@ namespace AdressBook
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frm_main());
         }
+        internal static List<>
     }
 }
