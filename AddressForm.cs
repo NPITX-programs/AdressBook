c8e873f95614eb013648e5eeb90d9d4763e1e37e
CTH-npitx <chatch@npitx.org>
2025-04-21 10:42:33 -0500

    add stream writer (with try-catch)

--- AddressForm.cs
+++ AddressForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace AdressBook
         {
             InitializeComponent();
         }
-
+        string filepath = "placeholder";
         private void frm_main_Load(object sender, EventArgs e)
         {
 
@@ -24,7 +25,16 @@ namespace AdressBook
 
         private void WriteToFile()
         {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(filepath))
+                {
 
+                }
+            } catch
+            {
+
+            }
         }
     }
 }
