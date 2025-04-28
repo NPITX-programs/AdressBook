95f256bbd8099caba7a1167c7d71ff44c2f07603
Um... Figure it out! <33612115+CTH999@users.noreply.github.com>
2025-04-28 08:30:34 -0500

    Revert "refresh"
    This reverts commit fe4badc8fb04a153dbae71213b9ff080970159a0.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdressBook
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frm_main());
+        }
+        internal static List<Contact> contacts = new List<Contact>(); //makes a collection that refferences the class that will store information
+    }
+}
