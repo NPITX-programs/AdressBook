580fbd023188d4918403f35f4c31c559bbee948a
CTH-npitx <chatch@npitx.org>
2025-04-28 10:51:10 -0500

    remove clear button
    turns out the clear button is uneeded

--- AddressForm.Designer.cs
+++ AddressForm.Designer.cs
@@ -37,7 +37,6 @@
             this.txt_phoNum = new System.Windows.Forms.MaskedTextBox();
             this.txt_lastName = new System.Windows.Forms.TextBox();
             this.btn_add = new System.Windows.Forms.Button();
-            this.btn_clear = new System.Windows.Forms.Button();
             this.btn_close = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -111,15 +110,6 @@
             this.btn_add.Text = "button1";
             this.btn_add.UseVisualStyleBackColor = true;
             // 
-            // btn_clear
-            // 
-            this.btn_clear.Location = new System.Drawing.Point(138, 125);
-            this.btn_clear.Name = "btn_clear";
-            this.btn_clear.Size = new System.Drawing.Size(75, 23);
-            this.btn_clear.TabIndex = 9;
-            this.btn_clear.Text = "button2";
-            this.btn_clear.UseVisualStyleBackColor = true;
-            // 
             // btn_close
             // 
             this.btn_close.Location = new System.Drawing.Point(138, 211);
@@ -135,7 +125,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btn_close);
-            this.Controls.Add(this.btn_clear);
             this.Controls.Add(this.btn_add);
             this.Controls.Add(this.txt_lastName);
             this.Controls.Add(this.txt_phoNum);
@@ -164,7 +153,6 @@
         private System.Windows.Forms.MaskedTextBox txt_phoNum;
         private System.Windows.Forms.TextBox txt_lastName;
         private System.Windows.Forms.Button btn_add;
-        private System.Windows.Forms.Button btn_clear;
         private System.Windows.Forms.Button btn_close;
     }
 }
