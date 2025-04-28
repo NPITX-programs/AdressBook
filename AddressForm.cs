bf386655bd057b82f966b49a4bd139c8f05c3005
Um... Figure it out! <33612115+CTH999@users.noreply.github.com>
2025-04-28 08:34:46 -0500

    Revert "refresh"
    This reverts commit 33b87750f56a8aa3f50408eba7789c8717313d7a.

---
+++ AddressForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdressBook
+{
+    public partial class frm_main : Form
+    {
+        string sep = "~";
+        public frm_main()
+        {
+            InitializeComponent();
+        }
+        string filepath = "placeholder";
+        private void frm_main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WriteToFile()
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(filepath))
+                {
+                    foreach(var c in Program.contacts) //the loop for createing the contents which will be saved
+                    {
+                        //csv - comma seperated values
+                        //firstname-lastname-email-phone-buisness-notes
+                        string line = c.firstname + sep + c.lastname + sep +  //first and last name
+                            c.email + sep + c.phone + sep + c.buisness + sep + //contact information
+                            c.notes; //notes
+                        sw.Write(line); //write the information to the line
+                    }
+                }
+            } catch(Exception ex)
+            {
+                MessageBox.Show("error" + ex.Message); //show error
+            }
+        }
+    }
+}
