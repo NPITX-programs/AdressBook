546468439a90a4dfe3eb00a833484a9cd7063d6a
CTH-npitx <chatch@npitx.org>
2025-05-05 12:03:23 -0500

    Calculate Ind
    Calculate the index. Again. had to do this so I would be able to merge it in elsewhen

--- AddressForm.cs
+++ AddressForm.cs
@@ -62,7 +62,7 @@ namespace AdressBook
             int leng = Program.contacts.Count; //get the length
             int ind = leng - 1; //get the index (a.k.a the length minus 1)
             Contact curr = Program.contacts[ind]; //get the item at said index, A.K.A. the last item) (curr is for "current")
-            return curr; //output that value
+            return curr; //output that value 
         }
         
 
@@ -71,14 +71,20 @@ namespace AdressBook
             string first = value.firstname; //get last name
             string last = value.lastname; //get first name
             string entry = first + " " + last; //combine first and last name
+
+            int leng = 0; //create length var
+            int ind = 0; //create index var
             
             if (value.buisness == true) //check contact type
             {
                 lst_buisnes.Items.Add(entry); //if it's buisness, add to buisness list
+                leng = lst_buisnes.Items.Count;
             } else
             {
                 lst_personal.Items.Add(entry); //if it's not buisness, add to personal list
+                leng = lst_personal.Items.Count;
             }
+            ind = leng - 1;
         }
 
         private void btn_add_Click(object sender, EventArgs e)
