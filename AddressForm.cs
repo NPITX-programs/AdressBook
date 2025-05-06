6cb0b64dec95d0c1637e061667b7af48fd356b1e
Um... Figure it out! <33612115+CTH999@users.noreply.github.com>
2025-05-06 15:51:35 -0500

    format

--- AddressForm.cs
+++ AddressForm.cs
@@ -25,8 +25,7 @@ namespace AdressBook
 
         private string CreatePath(string root, string path, string name, string extension)
         {
-            string finalPath = string.Empty;
-            finalPath = root + path + @"/" + name + "." + extension; //create the final path
+            string finalPath = root + path + @"/" + name + "." + extension; //create the final path
 
             return finalPath;
         }
@@ -34,7 +33,7 @@ namespace AdressBook
         const string name1 = "store";
         const string exten1 = "con";
         const int min = 6;
-        string direct = AppDomain.CurrentDomain.BaseDirectory + "contacts"; //create the directory path
+        const string direct = AppDomain.CurrentDomain.BaseDirectory + "contacts"; //create the directory path
         private void frm_main_Load(object sender, EventArgs e)
         {
             filepath = CreatePath(AppDomain.CurrentDomain.BaseDirectory,"contacts",name1,exten1);
