68540cc02246d230d058777e0f16392a956a12b8
Um... Figure it out! <33612115+CTH999@users.noreply.github.com>
2025-05-06 09:05:45 -0500

    Update AddressForm.cs

--- AddressForm.cs
+++ AddressForm.cs
@@ -146,7 +146,7 @@ namespace AdressBook
             }
         }
 
-        private Contact(string firstname, string lastname, string phoneNum, string EMail, string note)
+        private Contact newEntry(string firstname, string lastname, string phoneNum, string EMail, string note)
         {
             Contact c = new Contact();
 
@@ -155,9 +155,10 @@ namespace AdressBook
 
         private void btn_add_Click(object sender, EventArgs e)
         {
-            Contact c = new Contact();
-            bool valid = true;
             Contact entry = null;
+
+# region contact parts
+
 
             if (txt_firName.Text == string.Empty) //check if no firstname
             {
@@ -189,7 +190,7 @@ namespace AdressBook
                 c.buisness = chk_type.Checked;
                 Program.contacts.Add
 
-                entry = nextEntry();
+                entry = newEntry()
                 updateList(entry);
                 clear(); //clear inputs and set checkbox to false
                 WriteToFile(); //write to file
