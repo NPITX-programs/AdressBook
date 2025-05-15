e083034bb1eeaafb951bdcde89748a9387a7c20c
CTH-npitx <chatch@npitx.org>
2025-05-15 11:22:21 -0500

    create
    create the basis of the file

--- CodeFile1.cs
+++ CodeFile1.cs
@@ -1,4 +1,16 @@
-﻿public void WriteToFile2()
-{
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
+namespace AdressBook
+{
+    internal static class Files
+    {
+        static void Main()
+        {
+            MessageBox.Show("file not found"); //show error
+        }
+    }
 }
