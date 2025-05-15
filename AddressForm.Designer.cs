9050652f27ddceeae1e0d74b1c2609fb6f2bb4ab
CTH-npitx <chatch@npitx.org>
2025-05-15 10:43:31 -0500

    different tests
    -show always appears to do nothing, removed.
    -Remove title to de-clutter output temporarilly
    -Animation seems to have no effect, removed
    -Added icon to see effect

--- AddressForm.Designer.cs
+++ AddressForm.Designer.cs
@@ -331,8 +331,8 @@
             // 
             // toolTip2
             // 
-            this.toolTip2.ShowAlways = true;
-            this.toolTip2.ToolTipTitle = "Test This";
+            this.toolTip2.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.toolTip2.UseAnimation = false;
             this.toolTip2.UseFading = false;
             // 
             // AddressForm
