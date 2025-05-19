c0517de2fbf8b1eba857f3cf9b6b2d58a21eae59
CTH-npitx <chatch@npitx.org>
2025-05-19 11:36:26 -0500

    Revert "Update AddressForm.cs"
    This reverts commit 93cde57aa18f22d28155f4e44bb9145ffa031247.

--- AddressForm.cs
+++ AddressForm.cs
@@ -133,7 +133,17 @@ namespace AdressBook
         }
         private Contact createCont(string firstName, string lastName, string phoNum, string eMail, string contNote, bool contType)
         {
-
+            Contact c = new Contact //make new constact
+            {
+                firstname = firstName, //put the realevent input into the releavent field
+                lastname = lastName, //same
+                phone = phoNum, //same
+                email = eMail, //same
+                notes = contNote, //same
+                buisness = contType //same
+            };
+            Program.contacts.Add(c); //add to list
+            return c; //output the new contact
         }
 
         private void btn_add_Click(object sender, EventArgs e)
