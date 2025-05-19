f20fcafa550cb639cf287fec7757558f2db44b5a
Um... Figure it out! <33612115+CTH999@users.noreply.github.com>
2025-05-19 08:21:06 -0500

    Update AddressForm.Designer.cs

--- AddressForm.Designer.cs
+++ AddressForm.Designer.cs
@@ -272,6 +272,7 @@
             this.menuStrip1.Size = new System.Drawing.Size(800, 24);
             this.menuStrip1.TabIndex = 21;
             this.menuStrip1.Text = "menuStrip1";
+            this.toolTip2.SetToolTip(this.menuStrip1, "Control");
             // 
             // fileToolStripMenuItem
             // 
