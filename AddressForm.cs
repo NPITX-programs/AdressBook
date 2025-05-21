a4b6b16f782913dd84f33e3eadfadb5777281412
CTH-npitx <chatch@npitx.org>
2025-05-21 15:42:24 -0500

    comments

--- AddressForm.cs
+++ AddressForm.cs
@@ -21,7 +21,7 @@ namespace AdressBook //major updates needed
             InitializeComponent();
         }
 
-        string filepath = string.Empty;
+        string filepath = string.Empty; //a placeholder blank path
 
         private string CreatePath(string root, string path, string name, string extension)
         {
@@ -41,7 +41,7 @@ namespace AdressBook //major updates needed
             readFromFile(); //read from the file
             openFileDialog1.InitialDirectory = filepath; //set the initial directory to the base default file path
             saveFileDialog1.InitialDirectory = filepath; //set the initial directory to the default file path
-        }
+        } //when form loads
         int ind = 0; //index for the current contact
 
         private void readFromFile()
@@ -50,11 +50,11 @@ namespace AdressBook //major updates needed
             // call other file
             files.files.read(filepath, sep, min); //call the function used to read files
             generateList(); //generate the list, useing the default input of 0 (which will mean that it will run for the whole list. I think.
-        }
+        } //call the read from file method (that way I don't have to do file.files first)
         private void WriteToFile()
         {
             files.files.Write(filepath, sep); //call the function used to write files
-        }
+        } //call the write to file. Same as above
          
         private void clear() //triger to clear inputs
         {
@@ -75,9 +75,9 @@ namespace AdressBook //major updates needed
         }
         
 
-        private void updateList(Contact value)
+        private void updateList(Contact value) //update list
         {
-            dgv_contacts.Rows.Add( value.firstname, value.lastname, value.email, value.phone, value.buisness, value.notes);
+            dgv_contacts.Rows.Add( value.firstname, value.lastname, value.email, value.phone, value.buisness, value.notes); //update the data grid view with the contents of the contact
         }
         private void generateList(int total = 0) //generate the list, if no input it's assumed to use the entire length of the list
         {
@@ -95,7 +95,7 @@ namespace AdressBook //major updates needed
             for (int count = 0; count < total; count++)
             {
                 updateList(Program.contacts[ind]); //update the list
-                ind++; //
+                ind++; //increase index by one (apparently count doesn't increase)
             }
         }
         private Contact createCont(string firstName, string lastName, string phoNum, string eMail, string contNote, bool contType)
@@ -108,16 +108,16 @@ namespace AdressBook //major updates needed
                 email = eMail, //same
                 notes = contNote, //same
                 buisness = contType //same
-            };
+            }; //assemble contact
             Program.contacts.Add(c); //add to list
             return c; //output the new contact
         }
 
         private void btn_add_Click(object sender, EventArgs e)
         {
-            bool valid = true;
-            Contact entry = null;
-            Contact newContact = null;
+            bool valid = true; //default to valid
+            Contact entry = null; //temporary blank contact
+            Contact newContact = null; //temporary blank contact
 
             #region contactParts
             string firstName = txt_firName.Text; //first name
@@ -150,14 +150,14 @@ namespace AdressBook //major updates needed
             }
 
             if (valid == true || debug == true) {
-                newContact = createCont(firstName, lastName, phoNum, eMail, contNote, contType);
-                entry = nextEntry();
-                updateList(entry);
+                newContact = createCont(firstName, lastName, phoNum, eMail, contNote, contType); //create the contact
+                entry = nextEntry(); //get the next entry
+                updateList(entry); //update the list
                 clear(); //clear inputs and set checkbox to false
                 // WriteToFile(); 
                     //write to file
                     //disabled for now so it won't auto
-            }
+            } //the creation and setting code
         }//add contact
 
         private void btn_close_Click(object sender, EventArgs e) //hit closed button
