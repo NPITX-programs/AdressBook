2a64db99ca44666fd7adf61a8859f98a93c2c4fd
CTH-npitx <chatch@npitx.org>
2025-05-21 10:33:18 -0500

    removed list boxes

--- AddressForm.cs
+++ AddressForm.cs
@@ -231,34 +231,6 @@ namespace AdressBook //major updates needed
 
         private void select_contact(object sender, EventArgs e)
         {
-            ListBox lst = sender as ListBox; //get the list being used
-            int index = lst.SelectedIndex; //get the index of the current item
-            if (index == -1) //if no item is selected, clear it
-                {
-                    clear(); //clear form
-                } else {
-                    string id = lst.Tag.ToString(); //get the identifyer tag
-                    if (id == "per") //if it's personal
-                    {
-                        index = Program.perIndex[index]; //get the item at this index in the second list, which contains the indix of it's equivlent entry
-                    }
-                    else if (id == "bui") //if it's buisness
-                    {
-                        index = Program.buiIndex[index]; //get the item at this index in the second list, which contains the indix of it's equivlent entry
-                    }
-                    else
-                    {
-                        MessageBox.Show("error, tag not found"); //show error
-                    }
-                }
-            Contact c = Program.contacts[index]; //get the contact at the index
-
-            txt_firName.Text = c.firstname; //set the first name
-            txt_lastName.Text = c.lastname; //set the last name
-            txt_phoNum.Text = c.phone; //set the phone number
-            txt_EMail.Text = c.email; //set the email
-            txt_contNote.Text = c.notes; //set the notes
-            chk_type.Checked = c.buisness; //set the checkbox
 
         }
 
