ec5974029863efe8810e0edb0691a741a39d5999
CTH-npitx <chatch@npitx.org>
2025-06-11 10:20:52 -0500

    note on pos

--- AddressForm.cs
+++ AddressForm.cs
@@ -84,7 +84,7 @@ namespace AdressBook //major updates needed
             return curr; //output that value 
         } //gets the final entry in the list of contacts, which is the one that was just made (and thus the one that should be added)
         
-        private void updateList(Contact value) //pos: 225, 24
+        private void updateList(Contact value)
         {
             dgv_contacts.Rows.Add( value.firstname, value.lastname, value.email, value.phone, value.buisness, value.notes, value.index); //update the data grid view with the contents of the contact
         } //update list
