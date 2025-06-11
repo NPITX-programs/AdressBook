ba7b81ef4e0b2c6f2c6867dbf54630bbdccb934f
CTH-npitx <chatch@npitx.org>
2025-06-11 11:55:09 -0500

    Update AddressForm.Designer.cs

--- AddressForm.Designer.cs
+++ AddressForm.Designer.cs
@@ -255,7 +255,7 @@
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(350, 39);
+            this.panel1.Location = new System.Drawing.Point(400, 36);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(200, 100);
             this.panel1.TabIndex = 22;
