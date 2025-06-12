3d0c151a803d98d90ce18dbb89f04bb1a2095cf9
Um... Figure it out! <33612115+CTH999@users.noreply.github.com>
2025-06-12 10:36:58 -0500

    Edit input
    the button to edit the input. Probally will change to instead replace add entry?

--- AddressForm.Designer.cs
+++ AddressForm.Designer.cs
@@ -65,7 +65,7 @@
             this.col_contNotes = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.col_contInd = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.btn_clear = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btn_edit = new System.Windows.Forms.Button();
             this.groupBox2.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_contacts)).BeginInit();
@@ -382,21 +382,21 @@
             this.toolTip1.SetToolTip(this.btn_clear, "Clear Input");
             this.btn_clear.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btn_edit
             // 
-            this.button2.Location = new System.Drawing.Point(585, 85);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 23;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btn_edit.Location = new System.Drawing.Point(585, 85);
+            this.btn_edit.Name = "btn_edit";
+            this.btn_edit.Size = new System.Drawing.Size(75, 23);
+            this.btn_edit.TabIndex = 23;
+            this.btn_edit.Text = "Edit";
+            this.btn_edit.UseVisualStyleBackColor = true;
             // 
             // frm_addressForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(897, 450);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_edit);
             this.Controls.Add(this.btn_clear);
             this.Controls.Add(this.dgv_contacts);
             this.Controls.Add(this.groupBox2);
@@ -453,7 +453,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn col_contInd;
         private System.Windows.Forms.ToolStripMenuItem toolStrip_autoSave;
         private System.Windows.Forms.Button btn_clear;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btn_edit;
     }
 }
 
