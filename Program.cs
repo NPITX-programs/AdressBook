6fde77ddc62260a7b4fed6b64fa30210e75bf032
Um... Figure it out! <33612115+CTH999@users.noreply.github.com>
2026-08-11 15:03:00 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -151,7 +151,6 @@ namespace AdressBook
 
                 _conn.Close(); //close connection
             }
-            {
             catch (Exception ex) { //if an error
                 MessageBox.Show("Error Occured when attempting to edit Contents: " + ex.Message); //say what error is
                 if (_conn.State != ConnectionState.Closed) {
