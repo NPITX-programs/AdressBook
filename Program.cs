544b4de1ac861dcb6e7112d90d7076b4f85cd87c
Um... Figure it out! <33612115+CTH999@users.noreply.github.com>
2026-08-11 14:59:58 -0500

    set id

--- Program.cs
+++ Program.cs
@@ -137,7 +137,6 @@ namespace AdressBook
                 _conn = new SqlConnection(conString); //connect
                 _cmd = new SqlCommand(sqlString, _conn); //create command utilizing connection
 
-                _cmd.Parameters.AddWithValue("@id", id);
                 _cmd.Parameters.AddWithValue("@firstname", firstname);
                 _cmd.Parameters.AddWithValue("@lastname", lastname);
                 _cmd.Parameters.AddWithValue("@phone", phone);
@@ -145,6 +144,7 @@ namespace AdressBook
                 _cmd.Parameters.AddWithValue("@buisness", buisness);
                 _cmd.Parameters.AddWithValue("@notes", notes);
 
+                _cmd.Parameters.AddWithValue("@id", id); //use the method's ID input as the were
 
                 _conn.Open(); //open the connection
                 _cmd.ExecuteNonQuery(); //run the query
