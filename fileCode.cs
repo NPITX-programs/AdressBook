597fd014b0b845e61016dd7d293e1cbffd166518
CTH-npitx <chatch@npitx.org>
2026-09-03 11:21:26 -0500

    remove .
    remove the .files from the namespace

--- fileCode.cs
+++ fileCode.cs
@@ -3,8 +3,9 @@ using System.Diagnostics;
 using System.IO;
 using System.Windows.Forms;
 using System;
+using AdressBook;
 
-namespace AdressBook.files
+namespace AdressBook
 {
     internal static class files
     {
