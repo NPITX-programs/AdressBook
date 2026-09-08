7f09442d8b005c208076177dc013a32394ade9c3
CTH-npitx <chatch@npitx.org>
2026-09-08 09:55:16 -0500

    extraneus
    remove uneeded (currently) constant

--- Contact.cs
+++ Contact.cs
@@ -10,10 +10,7 @@
         internal string notes; //notes about contact
         internal int index; //the index of the entry
     }
-    internal class contConst
-    {
 
-    }
     internal partial class storageSystem
     {
         
