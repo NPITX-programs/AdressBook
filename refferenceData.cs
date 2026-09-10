98f775207bc72b8967419699ee7d7d0aec49404d
CTH-npitx <chatch@npitx.org>
2026-09-10 12:01:01 -0500

    Update refferenceData.cs

--- refferenceData.cs
+++ refferenceData.cs
@@ -1,7 +1,14 @@
-﻿namespace AdressBook
+﻿using System;
+
+namespace AdressBook
 {
-    internal class information
+    internal partial class information
     {
-        internal char csvSeperation = ',';
+        const char seperationChar = ',';
+        const string fileName = "store";
+        const string baseFolder = "contacts"; //name of file
+        const string fileExtension = "csv"; //extension of file
+        const int min = 6;//minimum total values in an entry
+        string root = AppDomain.CurrentDomain.BaseDirectory;
     }
 }
