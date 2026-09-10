7a69ba115a97481b968e7c3bdeff746f4889d003
CTH-npitx <chatch@npitx.org>
2026-09-10 11:49:17 -0500

    Update refferenceData.cs

--- refferenceData.cs
+++ refferenceData.cs
@@ -4,7 +4,12 @@
     {
         internal char csvSeperation = ',';
     }
+        private string CreatePath(string root, string path, string name, string extension)
+        {
             string finalPath = root + path + @"/" + name + "." + extension; //create the final path
+
+            return finalPath; //output said path
+        } //create the final directory path
         const string name1 = "store"; //name of file
         const string exten1 = "csv"; //extension of file
         const int min = 6; //minimum total values in an entry
