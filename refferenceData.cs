18939615d24aa97ddb55acc57acbb4fa2101b75a
CTH-npitx <chatch@npitx.org>
2026-09-10 11:49:02 -0500

    Update refferenceData.cs

--- refferenceData.cs
+++ refferenceData.cs
@@ -4,4 +4,9 @@
     {
         internal char csvSeperation = ',';
     }
+            string finalPath = root + path + @"/" + name + "." + extension; //create the final path
+        const string name1 = "store"; //name of file
+        const string exten1 = "csv"; //extension of file
+        const int min = 6; //minimum total values in an entry
+            filepath = CreatePath(AppDomain.CurrentDomain.BaseDirectory, "contacts", name1, exten1); //create the name, which is stored outside
 }
