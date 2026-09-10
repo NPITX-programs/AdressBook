01bc44b20fb2d704f50793c1adf92c3f0aa731db
CTH-npitx <chatch@npitx.org>
2026-09-10 12:00:53 -0500

    Update refferenceData.cs

--- refferenceData.cs
+++ refferenceData.cs
@@ -4,14 +4,4 @@
     {
         internal char csvSeperation = ',';
     }
-        private string CreatePath(string root, string path, string name, string extension)
-        {
-            string finalPath = root + path + @"/" + name + "." + extension; //create the final path
-
-            return finalPath; //output said path
-        } //create the final directory path
-        const string name1 = "store"; //name of file
-        const string exten1 = "csv"; //extension of file
-        const int min = 6; //minimum total values in an entry
-            filepath = CreatePath(AppDomain.CurrentDomain.BaseDirectory, "contacts", name1, exten1); //create the name, which is stored outside
 }
